91ae68e50ed5e32a7a2f98477642079461a5c9c5
MrJobsSa <jobssa439@gmail.com>
2026-04-12 17:21:44 +0200

    Updated db.js to use environment variables

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "jobsite"
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "",
+  database: process.env.DB_NAME || "jobsite",
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect((err) => {
