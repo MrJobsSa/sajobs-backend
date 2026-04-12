79c7aeff3a4643cdae8b36f0ccd5ad39ee5dc908
MrJobsSa <jobssa439@gmail.com>
2026-04-12 20:37:08 +0200

    Fixed port configuration

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use("/api/jobs", jobRoutes);
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
